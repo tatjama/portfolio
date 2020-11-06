70e6ee131f16b97a967b5f2fe8199f6c1ccd4f24
tatjama <tanja120a@gmail.com>
2020-11-06 14:48:55 +0100

    updated

--- js/animation.js
+++ js/animation.js
@@ -2,7 +2,7 @@
 
 const timeLine = gsap.timeline({defaults: {ease: "power1.out"}});
 
-timeLine.to(".text", {y: "0%", duration: 1, stagger: 0.35, delay: 1});
+timeLine.to(".text", {y: "0%", duration: 1, stagger: 0.5, delay: 2});
 timeLine.to(".slider", {y: "-100%", duration: 1.5, delay: 1});
 timeLine.to(".intro", {y: "-100%", duration: 1}, "-=1");
 timeLine.fromTo("#big-text", {opacity: 0}, {opacity: 1, duration: 1});
